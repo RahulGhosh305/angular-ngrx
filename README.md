571ac8e1024b7f90269972dad42922b1b56b65ce
Rahul Ghosh <rahulghosh096@gmail.com>
2024-10-28 17:35:55 +0600

    set ngrx dev tools

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# angular-ngrx
+# Angular NgRx
+
 State Management System
+
+## Requrirement
+
+- node v16
+- angular cli 8.3.29
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CounterButtonComponent } from "./components/counter-button/counter-button.component";
@@ -24,6 +25,10 @@ import { counterReducer } from "./store/counter.reducers";
     StoreModule.forRoot({ firstReducerProvider: counterReducer }),
     BrowserAnimationsModule,
     MaterialModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
